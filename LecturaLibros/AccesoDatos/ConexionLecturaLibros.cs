e68e709224132af1421b3bd6d3ef056b12818d51
Daniel Esparza <danielesparzza@gmail.com>
2020-10-08 20:14:14 -0500

    Se creó la clase ConexionLecturaLibros y la clase Program en la capa AccesoDatos

---
+++ LecturaLibros/AccesoDatos/ConexionLecturaLibros.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using Bases;
+
+namespace AccesoDatos
+{
+    public class ConexionLecturaLibros
+    {
+        Conectar c = new Conectar("localhost", "root", "1234567890", "LecturaLibros");
+
+        //Metodo para insertar, eliminar, modificar
+        public string Comando(string q)
+        {
+            return c.Comando(q);
+        }
+
+        //Metodo para consultar
+        public DataSet Mostrar(string q, string tabla)
+        {
+            return c.Consultar(q, tabla);
+        }
+    }
+}
